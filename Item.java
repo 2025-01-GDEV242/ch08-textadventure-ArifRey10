76ed4fa2c8e545ef65e5579cdeabdbaa54d03478
ArifRey10 <g00296272@stu.raritanval.edu>
2025-03-28 17:29:42 -0400

    added new class

---
+++ Item.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+
+/**
+ * Write a description of class Item here.
+ * These are the items of the Game
+ * @author Arif Reyhan
+ * @version 2025.03.28
+ */
+public class Item
+{
+    private String description;
+    private double weight;
+
+    public Item(String description, double weight) {
+        this.description = description;
+        this.weight = weight;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    @Override
+    public String toString() {
+        return description + " (Weight: " + weight + " kg)";
+    }
+}
+
+ 
